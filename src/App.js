0db9fe2142a35052a814d6b60c52a8b8d3eb158d
Ankitdotc <75359290+Ankitdotc@users.noreply.github.com>
2021-05-01 23:56:06 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -1,102 +1,108 @@
-import React from 'react';
-import SidebarComponent from './sidebar/sidebar';
-import EditorComponent from './editor/editor';
+
+import React from 'react'
 import './App.css';
+import firebase from "firebase";
+import "firebase/firestore";
+import SidebarComponent from './sidebar/sidebar'
+import EditorComponent from './editor/editor'
 
-
-const firebase = require('firebase');
 
 class App extends React.Component {
 
-  constructor() {
+  constructor(){
     super();
     this.state = {
       selectedNoteIndex: null,
-      selectedNote: null,
+      selectedNote: null, 
       notes: null
     };
   }
 
-  render() {
+  
+  componentDidMount = () => {
+    firebase
+    .firestore()
+    .collection('notes')
+    .onSnapshot(serverUpdate => {
+      const notes = serverUpdate.docs.map(_doc => {
+        const data = _doc.data();
+        data['id'] = _doc.id;
+        return data;
+      });
+      // console.log(notes)
+      this.setState({notes: notes})
+    });
+  }
+  
+    
+    render(){
     return(
       <div className="app-container">
-        {/* <h2><strong>   EVERNOTE   </strong></h2> */}
-        <h3><small>    make note making simple!   </small></h3>
+         <h3><small>    make note making simple!   </small></h3>
         <SidebarComponent 
-          selectedNoteIndex={this.state.selectedNoteIndex}
+          selectedNoteIndex={this.state.selectedNoteIndex} 
           notes={this.state.notes}
           deleteNote={this.deleteNote}
           selectNote={this.selectNote}
-          newNote={this.newNote}></SidebarComponent>
+          newNote={this.newNote}
+          />
         {
           this.state.selectedNote ?
-          <EditorComponent selectedNote={this.state.selectedNote}
-          selectedNoteIndex={this.state.selectedNoteIndex}
-          notes={this.state.notes}
-          noteUpdate={this.noteUpdate}></EditorComponent> :
-          null
+          <EditorComponent 
+            selectedNote={this.state.selectedNote}
+            selectedNoteIndex={this.state.selectedNoteIndex}
+            notes={this.state.notes}
+            noteUpdate={this.noteUpdate}
+          /> : null
         }
+        {/* if no note is selectd then it will not render */}
       </div>
-    );
+    )
   }
-    
-
-  componentDidMount = () => {
-    firebase
-      .firestore()
-      .collection('notes')
-      .onSnapshot(serverUpdate => {
-        const notes = serverUpdate.docs.map(_doc => {
-          const data = _doc.data();
-          data['id'] = _doc.id;
-          return data;
-        });
-        console.log(notes);
-        this.setState({ notes: notes });
-      });
+  selectNote = (note,index) => {
+    this.setState({selectedNoteIndex: index, selectedNote: note})
   }
-
-  selectNote = (note, index) => this.setState({ selectedNoteIndex: index, selectedNote: note });
   noteUpdate = (id, noteObj) => {
     firebase
-      .firestore()
-      .collection('notes')
-      .doc(id)
-      .update({
-        title: noteObj.title,
-        body: noteObj.body,
-        timestamp: firebase.firestore.FieldValue.serverTimestamp()
-      });
+    .firestore()
+    .collection('notes')
+    .doc(id)
+    .update({
+      title: noteObj.title,
+      body: noteObj.body,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    })
   }
   newNote = async (title) => {
     const note = {
-      title: title,
+      title: title, 
       body: ''
-    };
+    }
     const newFromDB = await firebase
-      .firestore()
-      .collection('notes')
-      .add({
-        title: note.title,
-        body: note.body,
-        timestamp: firebase.firestore.FieldValue.serverTimestamp()
-      });
+    .firestore()
+    .collection('notes')
+    .add({
+      title: note.title,
+      body: note.body,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    })
     const newID = newFromDB.id;
-    await this.setState({ notes: [...this.state.notes, note] });
+    await this.setState({ notes: [...this.state.notes, note] })
+
     const newNoteIndex = this.state.notes.indexOf(this.state.notes.filter(_note => _note.id === newID)[0]);
-    this.setState({ selectedNote: this.state.notes[newNoteIndex], selectedNoteIndex: newNoteIndex });
+    this.setState({selectedNote: this.state.notes[newNoteIndex], selectedNoteIndex: newNoteIndex});
   }
   deleteNote = async (note) => {
     const noteIndex = this.state.notes.indexOf(note);
-    await this.setState({ notes: this.state.notes.filter(_note => _note !== note) });
-    if(this.state.selectedNoteIndex === noteIndex) {
-      this.setState({ selectedNoteIndex: null, selectedNote: null });
+    await this.setState({ notes: this.state.notes.filter(_note=> _note !== note)})
+    if(this.state.selectedNoteIndex === noteIndex){
+      this.setState({ selectedNoteIndex: null, selectNote: null})
     } else {
-      this.state.notes.length > 1 ?
-      this.selectNote(this.state.notes[this.state.selectedNoteIndex - 1], this.state.selectedNoteIndex - 1) :
-      this.setState({ selectedNoteIndex: null, selectedNote: null });
+      this.state.notes.length > 1 ? 
+      this.selectNote(this.state.notes[this.state.selectedNoteIndex - 1], this.state.selectedNoteIndex - 1)
+      : 
+      this.setState({ selectedNoteIndex: null, selectNote: null})
     }
-
     firebase
       .firestore()
       .collection('notes')
@@ -104,5 +110,4 @@ class App extends React.Component {
       .delete();
   }
 }
-
-export default App;
+  export default App;
